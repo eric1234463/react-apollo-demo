0904066f47ebdc5b2c2e2aac36141821cfa14a6c
eric1234463 <eric1234463@gmail.com>
2019-10-10 22:09:45 +0800

    feat(react): add count

--- src/View.jsx
+++ src/View.jsx
@@ -3,10 +3,19 @@ import { useQuery } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
 
 const MORE_FILMS_QUERY = gql`
-  query AllFirms($offset: Int) {
-    allFilms(first: $offset) {
+  query AllFirms($limit: Int, $offset: Int) {
+    allFilms(first: $limit, skip: $offset) {
       id
       director
+      title
+    }
+  }
+`;
+
+const FILMS_COUNT_QUERY = gql`
+  {
+    _allFilmsMeta {
+      count
     }
   }
 `;
@@ -14,7 +23,19 @@ const MORE_FILMS_QUERY = gql`
 const View = () => {
   const { loading, error, data, fetchMore } = useQuery(MORE_FILMS_QUERY, {
     variables: {
-      offset: 10,
+      offset: 0,
+      limit: 1,
+    },
+  });
+
+  const {
+    loading: countLoading,
+    error: countError,
+    data: countData,
+  } = useQuery(FILMS_COUNT_QUERY, {
+    variables: {
+      offset: 0,
+      limit: 1,
     },
   });
 
@@ -23,6 +44,7 @@ const View = () => {
       query: MORE_FILMS_QUERY,
       variables: {
         offset: data.allFilms.length,
+        limit: 1,
       },
       updateQuery: (prev, { fetchMoreResult }) => {
         if (!fetchMoreResult) return prev;
@@ -33,19 +55,24 @@ const View = () => {
     });
   };
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+  if (loading || countLoading) return <p>Loading...</p>;
+  if (error || countError) return <p>Error :(</p>;
 
   return (
     <div>
-      {data.allFilms.map(({ id, director }) => (
+      Total: {countData._allFilmsMeta.count}
+      {data.allFilms.map(({ id, director, title }) => (
         <div key={id}>
           <p>
-            {id}: {director}
+            {id}: {director} - {title}
           </p>
         </div>
       ))}
-      <button type="button" onClick={loadMore}>
+      <button
+        type="button"
+        onClick={loadMore}
+        disabled={data.allFilms.length - 1 === countData._allFilmsMeta.count}
+      >
         Show More
       </button>
     </div>
